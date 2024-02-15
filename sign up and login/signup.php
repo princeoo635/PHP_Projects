188c660ace710abfaace826743b5bb294e0d31c4
princeoo635 <princeoo635@gmail.com>
2024-02-15 11:45:51 +0530

    added the confirm password feilds

--- sign up and login/signup.php
+++ sign up and login/signup.php
@@ -1,26 +1,33 @@
 <?php  
-$user=$_POST['user'];
-$password=$_POST['password'];
 $servername="localhost";
 $username="root";
 $dbpasssword="";
 $dbname="prince";
- 
-$conn= new mysqli($servername,$username,$dbpasssword,$dbname);
-if($conn->connect_error){
-    die("connection failed:".$conn->connect_error);
-}
-else
-{
-    echo "successfully connected";
-    $qry="insert into login(username,password) values(?,?)";
-    $stmt=$conn->prepare($qry);
-    $stmt->bind_param("ss",$user,$password);
-    $stmt->execute();
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $user=$_POST['user'];
+    $password=$_POST['password'];
+    $cpassword=$_POST['cpassword'];
     
-    
-    header('location:login.html');
+    $conn= new mysqli($servername,$username,$dbpasssword,$dbname);
+    if($conn->connect_error){
+        die("connection failed:".$conn->connect_error);
+    }
+    else
+    {
+       if( $password== $cpassword)
+       {
+           $qry="insert into login(username,password) values(?,?)";
+           $stmt=$conn->prepare($qry);
+           $stmt->bind_param("ss",$user,$password);
+           $stmt->execute();   
+           header('location:login.html');
+        }
+        else{
+            echo "Enter the same password";
+        }
+    }    
 }
-
-
+else{
+    echo "Invalid Request Method";
+}
 ?>
